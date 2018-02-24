ac3846d5676d1f9c46e99242c070c2b58ae8f1fc
katherinebai <36399340+katherinebai@users.noreply.github.com>
2018-02-24 13:58:41 +0800

    upload set focus command codes

---
+++ Resources/SetFocusCommand.js
@@ -0,0 +1,40 @@
+﻿var SetFocusCommand = (function (_super) {
+    __extends(SetFocusCommand, _super);
+    function SetFocusCommand() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+
+    SetFocusCommand.prototype.execute = function () {
+        var param = this.CommandParam;
+        var targetCellFormula = param.TargetCell;
+
+        var cellLocation = this.getCellLocation(targetCellFormula);
+        this.SetFocusToDom(this, cellLocation, 0);
+    }
+
+    SetFocusCommand.prototype.SetFocusToDom = function (self, cellLocation, retryTime) {
+        //PageloadedCommand set focus does not work, so try 10 times.
+        if (retryTime > 10)
+        {
+            return;
+        }
+
+        var cell = Forguncy.Page.getCellByLocation(cellLocation);
+        if (!cell) {
+            return;
+        }
+
+        //set focus
+        cell.setFocus();
+
+        if (!cell.hasFocus()) {
+            retryTime++;
+            Forguncy.DelayRefresh.setTimeout(function () {
+                self.SetFocusToDom(self, cellLocation, retryTime);
+            }, 100);
+        }
+    }
+    return SetFocusCommand;
+}(Forguncy.CommandBase));
+
+Forguncy.CommandFactory.registerCommand("SetFocusCommand.SetFocusCommand, SetFocusCommand", SetFocusCommand);
---
+++ SetFocusCommand.cs
@@ -0,0 +1,45 @@
+﻿using GrapeCity.Forguncy.Commands;
+using GrapeCity.Forguncy.Plugin;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetFocusCommand
+{
+    [Icon("pack://application:,,,/SetFocusCommand;component/Resources/icon.png")]
+    public class SetFocusCommand : Command
+    {
+        [FormulaProperty(true)]
+        [ResourcesDisplayName("SetFocusCommand_TargetCell")]
+        public object TargetCell { get; set; }
+
+        public override string ToString()
+        {
+            return Properties.Resources.SetFocusCommand_DisplayName;
+        }
+
+        public override CommandScope GetCommandScope()
+        {
+            return CommandScope.Cell | CommandScope.ListView | CommandScope.PageLoad;
+        }
+    }
+
+    public class ResourcesDisplayNameAttribute : DisplayNameAttribute
+    {
+        public ResourcesDisplayNameAttribute(string displayName)
+            :base(displayName)
+        {
+        }
+
+        public override string DisplayName
+        {
+            get
+            {
+                return Properties.Resources.ResourceManager.GetString(base.DisplayName);
+            }
+        }
+    }
+}
